adc21bdc966bbdf16592ed646f0554ff86052ab0
garyhui <gary.hui@sendum.com>
2023-08-14 20:24:35 -0700

    Added timestamp of todo at creation into db

--- server/database.sql
+++ server/database.sql
@@ -5,5 +5,6 @@ CREATE TABLE todolist(
     description VARCHAR(255)
     isEditing boolean NOT NULL
     isComplete boolean NOT NULL
+    creation_time timestamp with time zone DEFAULT current_timestamp
     
 )
